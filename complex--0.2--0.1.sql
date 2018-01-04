cf61cd083d7630c3cc3df1bdfefee31b8fd5fe6d
itambouras <itambouras@users.noreply.github.com>
2018-01-04 04:48:57 -0800

    init

--- complex--0.2--0.1.sql
+++
@@ -1,8 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION complex" to load this file. \quit
-
-
-DROP OPERATOR IF EXISTS && ;
-
-DROP OPERATOR CLASS IF EXISTS  complex_inclusion_ops USING brin;
-
